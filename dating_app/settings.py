65e1f720dd4584e82c20ca55461a962eafc11c10
Adarsh P Baiju <adarshpbaiju2060@gmail.com>
2024-08-23 12:49:14 +0530

    "Updated settings.py for static files, removed index.html from jobPortal templates, and modified TestView in jobPortal views to render a different template."

--- dating_app/settings.py
+++ dating_app/settings.py
@@ -117,8 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATICFILES_DIRS = [STATIC_DIR]
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- jobPortal/views.py
+++ jobPortal/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def TestView(request):
-    return render(request, 'index.html')
+    return render(request, 'JobPortal/EmployerDashboard/dashboard.html')
